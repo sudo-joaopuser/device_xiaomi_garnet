d3dea8e442d23376d0c1b1ca654b0a4e9a52d89e
Cosmin Tanislav <demonsingur@gmail.com>
2025-10-05 23:52:36 +0530

    garnet: build audio blobs from source
    Change-Id: Ia2d82e4252fead1dbd6b3267abc83b161b0b5712

--- device.mk
+++ device.mk
@@ -57,13 +57,28 @@ PRODUCT_PACKAGES += \
     audio.usb.default
 
 PRODUCT_PACKAGES += \
+    agmcap \
+    agmcompressplay \
+    agmplay \
     libaudioroute-v34 \
+    lib_bt_aptx \
+    lib_bt_ble \
+    lib_bt_bundle \
+    libagm_compress_plugin \
+    libagm_mixer_plugin \
+    libagm_pcm_plugin \
+    libagmclient \
     libbatterylistener \
     libfmpal \
     libqcompostprocbundle \
     libqcomvisualizer \
     libqcomvoiceprocessing \
+    libsndcardparser \
     libvolumelistener
+
+PRODUCT_PACKAGES += \
+    vendor.qti.hardware.AGMIPC@1.0.vendor \
+    vendor.qti.hardware.pal@1.0.vendor
 
 AUDIO_HAL_DIR := hardware/qcom-caf/sm8450/audio/primary-hal
 
